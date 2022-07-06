74e11c34d75aa355516f27b9ac6e37c9c23bae0b
makeme666 <108784390+makeme666@users.noreply.github.com>
2022-07-07 00:50:52 +0800

    Update 1clone.sh

--- preset-models/1clone.sh
+++ preset-models/1clone.sh
@@ -10,5 +10,5 @@ cd package/myapps
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
 git clone https://github.com/jerrykuku/luci-app-argon-config.git
 git clone https://github.com/kenzok8/openwrt-packages.git
-git clone https://github.com/Ausaci/luci-app-nat6-helper.git
+# git clone https://github.com/Ausaci/luci-app-nat6-helper.git
 git clone -b packages --single-branch https://github.com/xiaorouji/openwrt-passwall.git
